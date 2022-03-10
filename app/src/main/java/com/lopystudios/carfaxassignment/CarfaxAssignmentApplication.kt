926a92a57fba457bbb192951ff34cf05ab01ba50
jasonrch <151870228+jasonrch@users.noreply.github.com>
2022-03-10 00:01:58 -0500

    feat: add image caching

--- app/src/main/java/com/lopystudios/carfaxassignment/CarfaxAssignmentApplication.kt
+++ app/src/main/java/com/lopystudios/carfaxassignment/CarfaxAssignmentApplication.kt
@@ -1,7 +1,31 @@
 package com.lopystudios.carfaxassignment
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.annotation.ExperimentalCoilApi
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class CarfaxAssignmentApplication: Application()
+class CarfaxAssignmentApplication : Application(), ImageLoaderFactory {
+
+    @OptIn(ExperimentalCoilApi::class)
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(.20)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(this.cacheDir.resolve("image_cache"))
+                    .maxSizePercent(0.02)
+                    .build()
+            }
+            .crossfade(true)
+            .build()
+    }
+}
