25ab6c8827c642e71003294455ef405a8581ae4a
jasonrch <151870228+jasonrch@users.noreply.github.com>
2022-03-10 00:02:26 -0500

    feat: pass car object to detail screen and add on click actions for call dealer btn

--- app/src/main/java/com/lopystudios/carfaxassignment/presentation/car_details/CarDetailScreen.kt
+++ app/src/main/java/com/lopystudios/carfaxassignment/presentation/car_details/CarDetailScreen.kt
@@ -1,15 +1,47 @@
 package com.lopystudios.carfaxassignment.presentation.car_details
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.lopystudios.carfaxassignment.domain.model.Car
 import com.lopystudios.carfaxassignment.presentation.car_details.components.CarDetailComponent
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun CarDetailScreen() {
+fun CarDetailScreen(
+    car: Car? = null
+) {
+
+    val context = LocalContext.current
+    val phonePermissionState = rememberPermissionState(
+        Manifest.permission.CALL_PHONE
+    )
 
     Box(modifier = Modifier.fillMaxSize()) {
-        CarDetailComponent()
+        if (car != null) {
+            CarDetailComponent(
+                car = car,
+                onCallDealerClick = {
+                    if (phonePermissionState.status.isGranted) {
+                        val intent = Intent(
+                            Intent.ACTION_CALL,
+                            Uri.parse("tel:" + car.dealerPhoneNumber)
+                        )
+                        context.startActivity(intent)
+
+                    } else {
+                        phonePermissionState.launchPermissionRequest()
+                    }
+                }
+            )
+        }
     }
 }
--- app/src/main/java/com/lopystudios/carfaxassignment/presentation/car_list/CarListScreen.kt
+++ app/src/main/java/com/lopystudios/carfaxassignment/presentation/car_list/CarListScreen.kt
@@ -1,30 +1,31 @@
 package com.lopystudios.carfaxassignment.presentation.car_list
 
-import androidx.compose.foundation.layout.*
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rxjava3.subscribeAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.lopystudios.carfaxassignment.data.remote.dto.CarFax
-import com.lopystudios.carfaxassignment.data.remote.dto.Listings
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.lopystudios.carfaxassignment.common.Constants.CAR_KEY
+import com.lopystudios.carfaxassignment.data.remote.dto.toCar
+import com.lopystudios.carfaxassignment.domain.model.Car
 import com.lopystudios.carfaxassignment.presentation.Screens
 import com.lopystudios.carfaxassignment.presentation.car_list.components.CarItemComponent
+import com.squareup.moshi.Moshi
+import java.net.URLEncoder
 
+
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun CarListScreen(
     navController: NavController,
@@ -33,13 +34,39 @@ fun CarListScreen(
 
     val items by viewModel.items.subscribeAsState(initial = emptyList())
 
+    val context = LocalContext.current
+    val phonePermissionState = rememberPermissionState(
+        android.Manifest.permission.CALL_PHONE
+    )
+
     Box(modifier = Modifier.fillMaxSize()) {
         LazyColumn(modifier = Modifier.fillMaxSize()) {
-            items(items) { listing ->
+            items(items.map { it.toCar() }) { car ->
+
+                val moshi = Moshi.Builder().build()
+                val jsonAdapter = moshi.adapter(Car::class.java).lenient()
+                val carJson = jsonAdapter.toJson(car)
+                val carJsonEncoded = URLEncoder.encode(carJson, "utf-8")
+
                 CarItemComponent(
-                    onItemClick = { /*TODO*/ },
-                    onCallDealerClick = { /*TODO*/ },
-                    listing = listing
+                    onItemClick = {
+                        navController.navigate(
+                            Screens.CarDetailScreen.route.replace("{$CAR_KEY}", carJsonEncoded)
+                        )
+                    },
+                    onCallDealerClick = {
+                        if (phonePermissionState.status.isGranted) {
+                            val intent = Intent(
+                                Intent.ACTION_CALL,
+                                Uri.parse("tel:" + car.dealerPhoneNumber)
+                            )
+                            context.startActivity(intent)
+
+                        } else {
+                            phonePermissionState.launchPermissionRequest()
+                        }
+                    },
+                    car = car
                 )
             }
         }
